c4e5fab2b77be0f750dcffa37fd8e76d6edd9722
Andrew <skat2006@bk.ru>
2022-05-25 23:51:17 +0300

    home work 5, 6 - Arrays

--- src/HomeWork6.java
+++ src/HomeWork6.java
@@ -1,22 +1,11 @@
 import java.text.DecimalFormat;
-import java.util.ArrayList;
 /*
 МАССИВЫ ч.2
  */
 public class HomeWork6 {
+    private static final int[] array = randomIntArray();
+
     public static void main(String[] args) {
-
-        int minValue = 100_000;
-        int maxValue = 200_000;
-        ArrayList<Integer> array = new ArrayList<>();
-
-        System.out.println("Случайные числа в массиве: ");
-        for (int i = 0; i < 30; i++) {
-            array.add((int) (Math.random() * ((maxValue - minValue) + 1)) + minValue);
-            System.out.print(array.get(i) + ",");
-        }
-        System.out.println("\n");
-
         // задача 1
         int totalSum = 0;
         for (Integer value : array) {
@@ -25,15 +14,15 @@ public class HomeWork6 {
         System.out.println("Сумма трат за месяц составила " + new DecimalFormat("###,###").format(totalSum) + " рублей\n");
 
         // задача 2
-        int maxNum = array.get(0);
-        int minNum = array.get(0);
+        int maxNum = array[0];
+        int minNum = array[0];
 
-        for (int i = 1; i < array.size(); i++) {
-            if (array.get(i) < minNum) {
-                minNum = array.get(i);
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < minNum) {
+                minNum = array[i];
             }
-            if (array.get(i) > maxNum) {
-                maxNum = array.get(i);
+            if (array[i] > maxNum) {
+                maxNum = array[i];
             }
         }
         System.out.println("Минимальная сумма трат за день составила " + minNum + " рублей.\nМаксимальная сумма трат за день составила " + maxNum + " рублей");
@@ -43,12 +32,23 @@ public class HomeWork6 {
         for (Integer value : array) {
             summary += value;
         }
-        System.out.println("\nСредняя сумма трат за месяц составила " + (summary/(array.size()-1)) + " рублей");
+        System.out.println("\nСредняя сумма трат за месяц составила " + (summary / (array.length)) + " рублей");
 
         // задача 4
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length-1; i > -1; i--) {
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i > -1; i--) {
             System.out.print(reverseFullName[i]);
         }
+    }
+
+    public static int[] randomIntArray() {
+        int[] arr = new int[30];
+        System.out.println("Случайные числа в массиве: ");
+        for (int i = 0; i < 30; i++) {
+            arr[i] = (int) (Math.random() * ((200000 - 100000) + 1)) + 100000;
+            System.out.print(arr[i] + ",");
+        }
+        System.out.println("\n");
+        return arr;
     }
 }
